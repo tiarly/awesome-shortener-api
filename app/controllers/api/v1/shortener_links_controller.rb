f652d06d75b4243c64da2f13b76926c37db5e53b
Tiarli Oliveira <tiarlioliveira@gmail.com>
2020-01-29 06:43:20 +0100

    fix: Resolve reek issues

--- app/controllers/api/v1/shortener_links_controller.rb
+++ app/controllers/api/v1/shortener_links_controller.rb
@@ -1,22 +1,28 @@
-class Api::V1::ShortenerLinksController < ApplicationController
-  def create
-    link = ShortenerLink.new(create_params)
+# frozen_string_literal: true
 
-    if ::ShortenerLinks::Creator.call(link)
-      render json: ShortenerLinkSerializer.new(link).attributes
-    else
-      render json: { errors: { **link.errors } }, status: :unprocessable_entity
+module Api
+  module V1
+    class ShortenerLinksController < ApplicationController
+      def create
+        link = ShortenerLink.new(create_params)
+
+        if ::ShortenerLinks::Creator.call(link)
+          render json: ShortenerLinkSerializer.new(link).attributes
+        else
+          render json: { errors: { **link.errors } }, status: :unprocessable_entity
+        end
+      end
+
+      def show
+        link = ::ShortenerLinks::Finder.call(params[:token])
+        redirect_to link.original_url
+      end
+
+      private
+
+      def create_params
+        params.require(:shortener_link).permit(:original_url)
+      end
     end
-  end
-
-  def show
-    link = ::ShortenerLinks::Finder.call(params[:token])
-    redirect_to link.original_url
-  end
-
-  private
-
-  def create_params
-    params.require(:shortener_link).permit(:original_url)
   end
 end
