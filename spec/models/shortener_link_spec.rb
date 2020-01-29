af2c927acc4b5f4d2386f6cfc910b2cd7394281d
Tiarli Oliveira <tiarlioliveira@gmail.com>
2020-01-29 06:23:38 +0100

    tests: Add missing specs for shortener_link

--- spec/models/shortener_link_spec.rb
+++ spec/models/shortener_link_spec.rb
@@ -9,4 +9,15 @@ RSpec.describe ShortenerLink, type: :model do
     it { expect(build(:shortener_link)).to be_valid }
     it { expect(build(:shortener_link, :with_invalid_url)).to_not be_valid }
   end
+
+  describe '.shortened_url' do
+    subject { link.shortened_url }
+
+    let(:link) { build(:shortener_link, token: token) }
+    let(:token) { 'mytoken' }
+
+    it 'returns the token url with the app domain' do
+      is_expected.to eq "http://localhost:3000/#{token}"
+    end
+  end
 end
